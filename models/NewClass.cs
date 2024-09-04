da11d20083783f57ae3bbf7f467eb88f5fed256c
Marcos Vinicius <vinioliveira2008y@gmail.com>
2024-09-04 15:38:54 -0300

    feat: acréscimo de um codig o

--- models/NewClass.cs
+++ models/NewClass.cs
@@ -8,27 +8,40 @@ namespace textetion_.models
     public class NewClass
     {
         private string _nome;
+        private int _idade;
         public string Nome 
-        {
-          get{
-            return _nome.ToUpper();
-          }
-         
-          set{
-            if (value == "")
+        { 
+            get => _nome.ToUpper();
+
+            set
             {
+              if(value == "")
+              {
                 throw new ArgumentException("O nome não pode ser vazio");
+              }
+              
+              _nome = value;
             }
+        }
 
-            _nome = value
-          } 
+        public string SobreNome { get; set; }
+        public string NomeCompleto => $"{Nome} {SobreNome}" .ToUpper();
 
-       }
-        public int Idade { get; set; }
 
+        public int Idade
+         {  get => _idade; 
+
+            set{
+              if(value < 0)
+              {
+                throw new ArgumentException("A idade não pode ser menor que zero");
+              }
+              _idade = value;
+            } 
+         }
         public void Apresentar()
         {
-            System.Console.WriteLine($"nome: {Nome}, idade: {Idade}");
+            System.Console.WriteLine($"nome: {NomeCompleto}, idade: {Idade}");
 
         }
     }
