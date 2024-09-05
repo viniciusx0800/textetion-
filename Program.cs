adfdb1949f991e80c16ad73d81b28191f5ec2586
Marcos Vinicius <vinioliveira2008y@gmail.com>
2024-09-05 16:33:58 -0300

    feat: acréscimo de uma funcionalidade

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
-﻿using textetion_.models;
+﻿using System.Globalization;
+using textetion_.models;
+// using System.Globalization;
 
-Pessoa p1 = new Pessoa(nome:"Leonardo", sobreNome:"Bau");
-// p1.Nome = "fred buta";
-// p1.SobreNome = "da Silva";
+ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-Pessoa p2 = new Pessoa(nome:"adelio", sobreNome:"roberto");
-// p2.Nome = "adelio";
-// p2.SobreNome = "roberto";
+decimal valorMonetario = 1582.40M;
 
+System.Console.WriteLine($"{valorMonetario:C}");
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
-
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+// System.Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+System.Console.WriteLine(valorMonetario.ToString("C4"));
+                                                //   N2 
 
 
 
@@ -34,16 +29,35 @@ cursoDeIngles.ListarAlunos();
 
 
 
+// int numero1 = 10;
+// string numero2 = "20";
+
+// string resultado = numero1 + numero2;
+
+// System.Console.WriteLine(resultado);
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
+// Pessoa p1 = new Pessoa(nome:"Leonardo", sobreNome:"Bau");
+// // p1.Nome = "fred buta";
+// // p1.SobreNome = "da Silva";
+
+// Pessoa p2 = new Pessoa(nome:"adelio", sobreNome:"roberto");
+// // p2.Nome = "adelio";
+// // p2.SobreNome = "roberto";
 
 
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
-
-
-
-
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "fred buta";
