257e5431c39f4b20ce37be0adbb681302901f239
Marcos Vinicius <vinioliveira2008y@gmail.com>
2024-09-05 15:39:07 -0300

    adicionei número 1

--- models/Curso.cs
+++ models/Curso.cs
@@ -34,7 +34,7 @@ namespace textetion_.models
             for(int count = 0; count < Alunos.Count; count++)
             {
                 // string texto = "N° " + count + " - " + Alunos[count].NomeCompleto;
-                string texto = $"N°  {count} - {Alunos[count].NomeCompleto}" ; 
+                string texto = $"N°  {count + 1} - {Alunos[count].NomeCompleto}" ; 
                 System.Console.WriteLine(texto);
             }
             // foreach(Pessoa aluno in Alunos)
